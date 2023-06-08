cd406b3c27224a73fd83387b06e738d53c276b92
JoshuaPagel <128010152+JoshuaPagel@users.noreply.github.com>
2023-06-08 12:17:56 -0700

    yes

--- Python/fundamentals/hello_world.py
+++ Python/fundamentals/hello_world.py
@@ -1,13 +1,13 @@
 # # 1. TASK: print "Hello World"
-# print("hello world")
+print("hello world")
 # # 2. print "Hello Noelle!" with the name in a variable
-# name = "Noelle"
-# print("hello", name)	# with a comma
-# print("hello " + name)	# with a +
+name = "Noelle"
+print("hello", name)	# with a comma
+print("hello " + name)	# with a +
 # # 3. print "Hello 42!" with the number in a variable
-# name = 42
-# print("hello", int(name))	# with a comma
-# print("hello " + int(name))	# with a +	-- this one should give us an error!
+name = 42
+print("hello", int(name))	# with a comma
+print("hello " + int(name))	# with a +	-- this one should give us an error!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
