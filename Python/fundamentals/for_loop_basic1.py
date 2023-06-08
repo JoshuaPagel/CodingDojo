fb287cf136ce0e0bb46d054640cc315d8805346e
JoshuaPagel <128010152+JoshuaPagel@users.noreply.github.com>
2023-06-08 14:33:44 -0700

    first commit

--- Python/fundamentals/for_loop_basic1.py
+++ Python/fundamentals/for_loop_basic1.py
@@ -1,29 +1,29 @@
 #1
-# for i in range(0,150):
-#     print(i)
+for i in range(0,150):
+    print(i)
 #2
-# for i in range(5,1000,5):
-#     print(i)
+for i in range(5,1000,5):
+    print(i)
 #3
-# for i in range(1,100):
-#     if i % 5 == 0:
-#         print("Coding")
-#     if i % 10 == 0:
-#         print("Coding Dojo")
-#     else:
-#         print(i)
+for i in range(1,100):
+    if i % 5 == 0:
+        print("Coding")
+    if i % 10 == 0:
+        print("Coding Dojo")
+    else:
+        print(i)
 #4
-# count = 0
-# for i in range(0,500000):
-#     if(i % 2 !=0):
-#         count += i
-# print(count)
+count = 0
+for i in range(0,500000):
+    if(i % 2 !=0):
+        count += i
+print(count)
 #5
-# count = 2018
-# while count > 0:
-#     count -= 4
-#     if count > 0:
-#         print(count)
+count = 2018
+while count > 0:
+    count -= 4
+    if count > 0:
+        print(count)
 #6
 lowNum = 2
 highNum = 9
