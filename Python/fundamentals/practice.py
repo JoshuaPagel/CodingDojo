2a81d59d43a299762261141ae141d41b3c309a82
JoshuaPagel <128010152+JoshuaPagel@users.noreply.github.com>
2023-06-09 11:29:03 -0700

    practice debugging

--- Python/fundamentals/practice.py
+++ Python/fundamentals/practice.py
@@ -1,5 +1,20 @@
-first_name = "Zen"
-last_name = "Coder"
-age = 27
-print(f"My name is {first_name} {last_name} and I am {age} years old.")
+# first_name = "Zen"
+# last_name = "Coder"
+# age = 27
+# print(f"My name is {first_name} {last_name} and I am {age} years old.")
 
+# def be_cheerful(name="Mr. Nibbles", repeat=2):
+#     print(f"Hey {name}\n" * repeat)
+
+# be_cheerful("Josh")
+# be_cheerful()
+# be_cheerful(repeat=4,name="Billy Bob")
+
+#debugging and multiplying
+def multiply(num_list, num):
+    for x in range(len(num_list)):
+        num_list[x] *= num
+    return num_list
+a = [2,4,10,16]
+b = multiply(a,5)
+print(b)
