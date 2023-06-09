9cdeaa8bdf05beb60654c3d1e94fb5aba1bfdf77
JoshuaPagel <128010152+JoshuaPagel@users.noreply.github.com>
2023-06-09 13:03:31 -0700

    fun basics 2 halfway

---
+++ Python/fundamentals/functions_basic_ii.py
@@ -0,0 +1,16 @@
+# #1
+# def countdown(x):
+#     print(x)
+#     while x >= 1:
+#         x -= 1
+#         print(x)
+# countdown(5)
+# #2
+# def print_and_return(a,b):
+#     print(a)
+#     return(b)
+# print_and_return(1,2)
+# #3
+def first_plus_length()
+    
+1,2,3,4,5
