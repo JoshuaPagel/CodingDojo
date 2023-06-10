faf17545de6dc25b308b80ce605b6dbfc1e05fa0
JoshuaPagel <128010152+JoshuaPagel@users.noreply.github.com>
2023-06-09 18:49:10 -0700

    attempt 2

--- Python/fundamentals/functions_basic_ii.py
+++ Python/fundamentals/functions_basic_ii.py
@@ -1,16 +1,47 @@
 # #1
-# def countdown(x):
-#     print(x)
-#     while x >= 1:
-#         x -= 1
-#         print(x)
-# countdown(5)
+# Create a function that accepts a number as an input.
+# Return a new list that counts down by one,
+# from the number (0th element) down to 0 (last element).
+
+def countDown(x):
+    listy = []
+    for i in range(x,-1,-1):
+        listy.append(i)
+    return listy
+print(countDown(5))
+
 # #2
-# def print_and_return(a,b):
-#     print(a)
-#     return(b)
-# print_and_return(1,2)
+# Create a function that will receive a list with two numbers.
+# Print the first and value and return the second .
+
+def print_and_return(a,b):
+    print(a)
+    return b
+print(print_and_return(1, 2))
 # #3
-def first_plus_length()
-    
-1,2,3,4,5
+# Create a function that accepts a list and returns the sum
+# of the first value in the list plus the list's length.
+
+def first_plus_length(myList):
+    return(myList[0] + len(myList))
+print(first_plus_length([1,2,3,4,5]))
+
+# #4
+# Write a function that accepts a list and 
+# creates a new list containing only the values from 
+# the original list that are greater than its 2nd value. 
+# Print how many values this is and then return the new list.
+# If the list has less than 2 elements, have the function return False
+
+def values_greater_than_second(forthList):
+        forthList = []
+        if len(forthList) == len(forthList[2]):
+            print("false")
+values_greater_than_second([5,2,3,2,1,4])
+
+
+# #5
+# Write a function that accepts two integers as parameters:
+# size and value. The function should create and return a list
+# whose length is equal to the given size, 
+# and whose values are all the given value.
