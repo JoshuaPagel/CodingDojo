37959f39992e024665ab5e090b59c9814a173849
JoshuaPagel <128010152+JoshuaPagel@users.noreply.github.com>
2023-06-14 13:08:20 -0700

    OOP dictionaries

--- Python/fundamentals/practice.py
+++ Python/fundamentals/practice.py
@@ -103,3 +103,19 @@ skater_shoe.on_sale_by_percent(0.2)
 
 # The skater shoes go on sale again by another 10%
 # skater_shoe.price = skater_shoe.price * (1 - 0.1)
+
+
+
+class Player:
+    def __init__(self, name, age, position, team):
+        self.name = name
+        self.age = age
+        self.position = position
+        self.team = team
+
+kevin = {"name": "Kevin Durant", "age":34, "position": "small forward", "team": "Brooklyn Nets"}
+
+# Pass in all the values from the dictionary by their keys
+player_kevin = Player(kevin["name"], kevin["age"], kevin["position"], kevin["team"])
+print(player_kevin.position) # prints small forward
+
