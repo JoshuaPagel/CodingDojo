83dd50a382fad88ab67f0d1e64a78864de664da0
JoshuaPagel <128010152+JoshuaPagel@users.noreply.github.com>
2023-07-08 20:10:47 -0700

    first commit

---
+++ first_flask.sql
@@ -0,0 +1,4 @@
+-- INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at)
+-- VALUES ('Jimmy', 'Johns', 'Food Worker', NOW(), NOW()), ('Bill', 'Johnson', 'Plumber', NOW(), NOW())
+
+SELECT * FROM friends
