85efca6be785c43e7fb5306f8b557da75cb961b9
JoshuaPagel <128010152+JoshuaPagel@users.noreply.github.com>
2023-07-13 21:02:57 -0700

    final commit

--- Python/flask_mysql/user_cr_assignment/flask_app/controllers/user_controller.py
+++ Python/flask_mysql/user_cr_assignment/flask_app/controllers/user_controller.py
@@ -9,35 +9,13 @@ def home():
 
 @app.route('/table')
 def table():
-    # user_id = users.create_user(request.form)
-    # session['user_id'] = user_id
-    # session['first_name'] = request.form['first_name']
-    # session['last_name'] = request.form['last_name']
-    # session['email'] = request.form['email']
-    # session['created_at'] = request.form['email']
-    return render_template ("table.html")
+    return render_template ("table.html", users=User.select_all_users())
 
 @app.route('/table/create')
 def add():
-    # first_name = request.form['first_name']
-    # last_name = request.form['last_name']
-    # email = request.form['email']
-    # created_at = request.form['created_at']
-    # user_id = users.create_user(request.form)
-    # session['user_id'] = request.form ['user_id']
-    # session['first_name'] = request.form['first_name']
-    # session['last_name'] = request.form['last_name']
-    # session['email'] = request.form['email']
-    # session['created_at'] = request.form['email']
     return render_template('sign_up.html')
 
 @app.route('/table/save', methods=['POST'])
 def save():
-    # first_name = request.form['first_name']
-    # last_name = request.form['last_name']
-    # email = request.form['email']
-    # created_at = request.form['created_at']
-    print(request.form)
-    User(request.form)
+    User.save_user(request.form)
     return redirect('/table')
-    # first_name=first_name, last_name=last_name, email=email, created_at=created_at)
--- Python/flask_mysql/user_cr_assignment/flask_app/models/user_model.py
+++ Python/flask_mysql/user_cr_assignment/flask_app/models/user_model.py
@@ -1,6 +1,6 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 
-# db = 'assignment_users'
+db = 'assignment_users'
 
 class User:
     def __init__(self, data):
@@ -14,25 +14,21 @@ class User:
 
 
     @classmethod
-    def select_all_users (cls):
+    def select_all_users(cls):
         query = "SELECT * FROM users;"
-        db_response = connectToMySQL("assignment_users").query_db(query)
+        db_response = connectToMySQL(db).query_db(query)
         users = []
         for user in db_response:
             users.append( cls(user) )
-            
-            # new_user = cls(user)
-            # return users
-        print(db_response)
         return users
     
 
     @classmethod
-    def save_user(cls, form_data):
-        query = "INSERT INTO users (first_name, last_name, email, created_at) = (%(first_name), %(last_name), %(email), %(created_at));"
+    def save_user(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email) VALUES (%(first_name)s, %(last_name)s, %(email)s);"
         # db_response = connectToMySQL('assignment_users').query_db(query, form_data)
         # print(db_response)
-        result = connectToMySQL('assignment_users').query_db(query, form_data)
+        result = connectToMySQL(db).query_db(query, data)
         return result
     
 # I'm stumped on connecting the database
